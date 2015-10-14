11b4228867b690bb4f79f3c67e7dc30605cef1cd
femanov <femanov>
2015-10-14 17:28:46 +0600

    name changed to pycx4, updatet setup to work with setuptools and PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools.extension import Extension
-from Cython.Build import cythonize
 import numpy
 import sys
 
@@ -19,7 +18,6 @@ def cxpath():
             return path2cx
     return None
 
-
 cxdir = cxpath()
 if cxdir is None:
     print('Error: Unable to locate CX')
@@ -29,36 +27,34 @@ cx4lib = cxdir +'/4cx/src/lib'
 cx4include = cxdir + '/4cx/src/include'
 
 USE_CYTHON = False
-if os.path.isfile('pycda.pyx'):
-    USE_CYTHON = True
-    ext = '.pyx'
-else:
-    ext = '.c'
+ext = '.c'
+for x in sys.argv:
+    if x == 'USE_CYTHON':
+        USE_CYTHON = True
+        ext = '.pyx'
 
 extensions = [
-              Extension('pycda', ['pycda'+ext],
-                        include_dirs=[numpy.get_include(),
-                                      cx4include],
-                        libraries=['cda', 'cx_async', 'useful', 'misc', 'cxscheduler'],
-                        library_dirs=[cx4lib + '/cda',
-                                      cx4lib + '/cxlib',
-                                      cx4lib + '/useful',
-                                      cx4lib + '/misc',
-                                      ],
-                        ),
-              Extension('qcda', ['qcda'+ext],
-                        include_dirs=[numpy.get_include(),
-                                      cx4include],
-                        libraries=['cda', 'cx_async', 'useful', 'misc', 'Qcxscheduler', 'QtCore'],
-                        library_dirs=[cx4lib + '/cda',
-                                      cx4lib + '/cxlib',
-                                      cx4lib + '/Qcxscheduler',
-                                      cx4lib + '/useful',
-                                      cx4lib + '/misc',
-                                      ],
-                       )
+    Extension('pycx4.pycda', ['./pycx4/pycda'+ext],
+              include_dirs=[numpy.get_include(), cx4include],
+              libraries=['cda', 'cx_async', 'useful', 'misc', 'cxscheduler'],
+              library_dirs=[cx4lib + '/cda',
+                            cx4lib + '/cxlib',
+                            cx4lib + '/useful',
+                            cx4lib + '/misc',
+                           ]
+              ),
+    Extension('pycx4.qcda', ['./pycx4/qcda'+ext],
+          include_dirs=[numpy.get_include(), cx4include],
+          libraries=['cda', 'cx_async', 'useful', 'misc', 'Qcxscheduler', 'QtCore'],
+          library_dirs=[cx4lib + '/cda',
+                        cx4lib + '/cxlib',
+                        cx4lib + '/Qcxscheduler',
+                        cx4lib + '/useful',
+                        cx4lib + '/misc',
+                       ]
+         )
+]
 
-              ]
 
 # Cython directives
 directives = {
@@ -77,10 +73,9 @@ if USE_CYTHON:
     from Cython.Build import cythonize
     extensions = cythonize(extensions, compiler_directives=directives)
 
-
 setup(
     name='pycx4',
-    version='0.16',
+    version='0.192',
     url='https://github.com/femanov/pycx4/wiki',
     download_url='https://github.com/femanov/pycx4',
     author='Fedor Emanov',
@@ -88,10 +83,14 @@ setup(
     license='GPL',
     description='CXv4 control system framework Python bindings',
     long_description='CXv4 control system framework Python bindings, pycda and qcda modules',
-    install_requires = [
+    install_requires=[
         "numpy >= 1.7",
         "PyQtX",
         ],
+    packages=['pycx4'],
+    package_data={'pycx4':['tests_exampls/*.*',
+                           'tests_exampls/cx_build/*.*',
+                           'tests_exampls/test_servers/*.*']},
     platforms='Linux',
     classifiers=[
         "Intended Audience :: Developers",
@@ -114,6 +113,6 @@ setup(
         "Topic :: Software Development",
     ],
 
-    ext_modules = extensions
+    ext_modules=extensions
 )
 
